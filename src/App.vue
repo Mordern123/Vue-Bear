7c1a020cb0d30643c38b49b204b466f11cfb456d
楊昀晟(EricYang) <ericyang@chipbond.com.tw>
2022-12-13 17:00:34 +0800

    feat:cute bear

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img
+    src="../src/assets/forest.jpg"
+    style="height: 100vh; width: 100%"
+  >
+  <div class="container">
+    <BearComponent />
+  </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import { Options, Vue } from "vue-class-component";
+import BearComponent from "./components/BearComponent.vue";
 
 @Options({
   components: {
-    HelloWorld,
+    BearComponent,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #658397;
+  overflow: hidden;
 }
 </style>
---
+++ src/components/BearComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="bear-text">
+    {{ bearText }}
+  </div>
+  <div class="toggleWrapper">
+    <input
+      type="checkbox"
+      class="dn"
+      id="dn"
+    />
+    <label
+      for="dn"
+      class="toggle"
+    >
+      <span class="toggle__handler"></span>
+    </label>
+    <div class="bear-body">
+      <span class="eye left"></span>
+      <span class="eye right"></span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      bearText: "我是一隻可愛熊熊",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../styles/bear.scss";
+</style>
